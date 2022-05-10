27ff69484bee6de5af156943f88bc9bbdba81153
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2022-05-10 10:23:31 -0300

    Imagens dos produtos adicionadas em código

--- src/global/data/appImages.js
+++ src/global/data/appImages.js
@@ -1,3 +1,15 @@
 export const appImages = {
-    pizza:require('../../assets/products/pizza.png')
+    arancino:require('../../assets/products/arancino.png'),
+    cannolo: require('../../assets/products/cannolo.png'),
+    gelato: require('../../assets/products/gelato.png'),
+    lasagna: require('../../assets/products/lasagna.png'),
+    'piadina romagnola': require('../../assets/products/piadina romagnola.png'),
+    pizza:require('../../assets/products/pizza.png'),
+    polpetone: require('../../assets/products/polpetone.png'),
+    porchetta: require('../../assets/products/porchetta.png'),
+    ravioli: require('../../assets/products/ravioli.png'),
+    risotto: require('../../assets/products/risotto.png'),
+    striffoli: require('../../assets/products/struffoli.png'),
+    tiramisu: require('../../assets/products/tiramisu.png'),
+    'trippa alla romana': require('../../assets/products/trippa alla romana.png')
 }
