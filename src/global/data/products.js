5648e0283351e7d262a948d71345da4dde570b41
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2022-05-10 10:30:06 -0300

    Produtos adicionados ao array global de produtos, porém sem descrição e preço

--- src/global/data/products.js
+++ src/global/data/products.js
@@ -1,10 +1,106 @@
 export const products = [
     {
+        id:'0',
+        name:'Arancino',
+        img:'arancino',
+        price:'19,99',
+        type:'snack',
+        description: 'A melhor da região'
+    },
+    {
         id:'1',
+        name:'Cannolo',
+        img:'cannolo',
+        price:'19,99',
+        type:'snack',
+        description: 'A melhor da região'
+    },
+    {
+        id:'2',
+        name:'Gelato',
+        img:'gelato',
+        price:'19,99',
+        type:'snack',
+        description: 'A melhor da região'
+    },
+    {
+        id:'3',
+        name:'Lasagna',
+        img:'lasagna',
+        price:'19,99',
+        type:'snack',
+        description: 'A melhor da região'
+    },
+    {
+        id:'4',
+        name:'Piadina Romagnola',
+        img:'piadina romagnola',
+        price:'19,99',
+        type:'snack',
+        description: 'A melhor da região'
+    },
+    {
+        id:'5',
         name:'Pizza',
         img:'pizza',
         price:'19,99',
         type:'snack',
         description: 'A melhor da região'
-    }
+    },
+    {
+        id:'6',
+        name:'Polpetone',
+        img:'polpetone',
+        price:'19,99',
+        type:'snack',
+        description: 'A melhor da região'
+    },
+    {
+        id:'7',
+        name:'Porchetta',
+        img:'porchetta',
+        price:'19,99',
+        type:'snack',
+        description: 'A melhor da região'
+    },
+    {
+        id:'8',
+        name:'Ravioli',
+        img:'ravioli',
+        price:'19,99',
+        type:'snack',
+        description: 'A melhor da região'
+    },
+    {
+        id:'9',
+        name:'Risotto',
+        img:'risotto',
+        price:'19,99',
+        type:'snack',
+        description: 'A melhor da região'
+    },
+    {
+        id:'10',
+        name:'Striffoli',
+        img:'striffoli',
+        price:'19,99',
+        type:'snack',
+        description: 'A melhor da região'
+    },
+    {
+        id:'11',
+        name:'Striffoli',
+        img:'striffoli',
+        price:'19,99',
+        type:'snack',
+        description: 'A melhor da região'
+    },
+    {
+        id:'12',
+        name:'Trippa Alla Romana',
+        img:'trippa alla romana',
+        price:'19,99',
+        type:'snack',
+        description: 'A melhor da região'
+    },
 ]
