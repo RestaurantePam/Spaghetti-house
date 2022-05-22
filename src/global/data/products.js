60f4f60d78a6a8d785655153a72e929e86a77a8e
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2022-05-22 18:08:30 +0000

    Adicionada descrição detalhada para a maior parte dos produtos

--- src/global/data/products.js
+++ src/global/data/products.js
@@ -6,8 +6,8 @@ export const products = [
         img:appImages['arancino'],
         price:'19,99',
         type:'snack',
-        description: 'Bolinho de risotto cremoso e crocante.',
-        info:'Este é um texto de exemplo para informações mais detalhadas sobre o produto'
+        description: 'Cremoso e crocante.',
+        info:'Arancino é um bolinho de risoto e açafrão, no qual fazemos sempre perfeito, crocante por fora e cremoso por dentro'
     },
     {
         id:'1',
@@ -15,8 +15,8 @@ export const products = [
         img:appImages['cannolo'],
         price:'19,99',
         type:'dessert',
-        description: 'Canolo de doce de leite e frutas.',
-        info:'Este é um texto de exemplo para informações mais detalhadas sobre o produto'
+        description: 'Uma mistura de doce de leite e frutas de dar água na boca',
+        info:'Um dos nosso melhores doces. O canolo são conchas de pastelaria recheadas com chocolate, doce de leite, entre outros recheio'
     },
     {
         id:'2',
@@ -24,8 +24,8 @@ export const products = [
         img:appImages['gelato'],
         price:'19,99',
         type:'dessert',
-        description: 'Gelato de frutas e nozes.',
-        info:'Este é um texto de exemplo para informações mais detalhadas sobre o produto'
+        description: 'Acompanhado por frutas e nozes.',
+        info:'Para quem está na dieta, o Gelato é uma das melhores opções, um sorvete maravilhoso com aroma e sabor de frutas e nozes e com uma taxa pequena de gordura.'
     },
     {
         id:'3',
@@ -33,8 +33,8 @@ export const products = [
         img:appImages['lasagna'],
         price:'19,99',
         type:'snack',
-        description: 'lasanha a bolonhesa.',
-        info:'Este é um texto de exemplo para informações mais detalhadas sobre o produto'
+        description: 'Diversos sabores em um único prato.',
+        info:'Lasanha: um prato amado por muitos. Queijos, molhos e carnes fazem esse prato ser um dos melhores em nosso restaurante.'
     },
     {
         id:'4',
@@ -42,8 +42,8 @@ export const products = [
         img:appImages['piadina_romagnola'],
         price:'19,99',
         type:'snack',
-        description: 'Piadina de frango com legumes.',
-        info:'Este é um texto de exemplo para informações mais detalhadas sobre o produto'
+        description: 'No formato de um sorriso, para agradar seu dia',
+        info:'No formato de um taco, Piadina é uma ótima entrada. Em nosso restaurante damos como referência nossa Piadina de frango maravilhosa.'
     },
     {
         id:'5',
@@ -52,7 +52,7 @@ export const products = [
         price:'19,99',
         type:'snack',
         description: 'Pizza de diversos sabores.',
-        info:'Este é um texto de exemplo para informações mais detalhadas sobre o produto'
+        info:'Pizza, a maior receita criada pelos italianos e aprimorada pelo nosso restaurante, uma massa arredondada que contém molhos, queijos e carnes.'
     },
     {
         id:'6',
@@ -61,7 +61,7 @@ export const products = [
         price:'19,99',
         type:'snack',
         description: 'Bolinho de carne recheado com queijo.',
-        info:'Este é um texto de exemplo para informações mais detalhadas sobre o produto'
+        info:'Um bolinho de carne recheado com queijo e presunto, e por fora apresentado com uma leve camada de queijo e molho de tomate caseiro.'
     },
     {
         id:'7',
@@ -69,8 +69,8 @@ export const products = [
         img:appImages['porchetta'],
         price:'19,99',
         type:'snack',
-        description: 'Barriga de porco com pernil.',
-        info:'Este é um texto de exemplo para informações mais detalhadas sobre o produto'
+        description: 'Carne de primeira qualidade em fatias deliciosas.',
+        info:'Nossa Porchetta é considerada a melhor da região, nossa barriga de porco recheada com pernil faz com que seja o prato perfeito para acompanhar um caneca de cerveja.'
     },
     {
         id:'8',
@@ -78,8 +78,8 @@ export const products = [
         img:appImages['ravioli'],
         price:'19,99',
         type:'snack',
-        description: 'Ravioli com legumes e carnes',
-        info:'Este é um texto de exemplo para informações mais detalhadas sobre o produto'
+        description: 'Pacotinhos de carne e muito sabor',
+        info:'Ravioli feito na hora, acompanhado de legumes, peixes, frangos e molhos é um excelente prato principal.'
     },
     {
         id:'9',
@@ -87,8 +87,8 @@ export const products = [
         img:appImages['risotto'],
         price:'19,99',
         type:'snack',
-        description: 'Risotto com queijos e carnes.',
-        info:'Este é um texto de exemplo para informações mais detalhadas sobre o produto'
+        description: 'Preenchendo a mesa com sabores únicos.',
+        info:'Em nosso risotto, colocamos arroz frito e cebola, carnes vermelhas ou brancas, queijos e um pouco de vinho.'
     },
     {
         id:'10',
@@ -96,8 +96,8 @@ export const products = [
         img:appImages['striffoli'],
         price:'19,99',
         type:'dessert',
-        description: 'Bolinhos de chuva italiano.',
-        info:'Este é um texto de exemplo para informações mais detalhadas sobre o produto'
+        description: 'Uma massa macia, irresistível e coberta de sabor.',
+        info:'Uma massa macia, irresistível e coberta de sabor.'
     },
     {
         id:'11',
@@ -106,7 +106,7 @@ export const products = [
         price:'19,99',
         type:'dessert',
         description: 'Camadas de puro sabor',
-        info:'Este é um texto de exemplo para informações mais detalhadas sobre o produto'
+        info:'Camadas de puro sabor'
     },
     {
         id:'12',
@@ -115,6 +115,6 @@ export const products = [
         price:'19,99',
         type:'snack',
         description: 'A dobradinha italiana',
-        info:'Este é um texto de exemplo para informações mais detalhadas sobre o produto'
+        info:'A dobradinha italiana'
     },
 ]
