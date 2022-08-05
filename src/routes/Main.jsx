70f2280532494b3246f4c58ed470b288fe0c8783
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2022-08-05 17:22:12 +0000

    Melhorado estilo da barra de navegação

--- src/routes/Main.jsx
+++ src/routes/Main.jsx
@@ -10,26 +10,38 @@ const stack = createBottomTabNavigator()
 
 function MainRoutes(){
     return (
-        <stack.Navigator screenOptions={{headerShown:false}}>
+        <stack.Navigator screenOptions={{headerShown:false, tabBarActiveTintColor:'red'}}>
             <stack.Screen 
                 name="home" 
                 component={Home} 
-                options={{tabBarIcon: (props) => <Icon {...props} name="home" />}}
+                options={{
+                    tabBarIcon: (props) => <Icon {...props} name="home" />,
+                    tabBarLabel:'Home'
+                }}
             />
             <stack.Screen 
                 name="products" 
                 component={ProductsRoutes}
-                options={{tabBarIcon: (props) => <Icon {...props} name="shopping" />}}
+                options={{
+                    tabBarIcon: (props) => <Icon {...props} name="shopping" />,
+                    tabBarLabel:'Produtos'
+                }}
             />
             <stack.Screen 
                 name="contact" 
                 component={Contact}
-                options={{tabBarIcon: (props) => <Icon {...props} name="phone" />}}
+                options={{
+                    tabBarIcon: (props) => <Icon {...props} name="phone" />,
+                    tabBarLabel:'Contato'
+                }}
             />
             <stack.Screen 
                 name="about" 
                 component={About} 
-                options={{tabBarIcon: (props) => <Icon {...props} name="face-profile" />}}
+                options={{
+                    tabBarIcon: (props) => <Icon {...props} name="face-profile" />,
+                    tabBarLabel:'Sobre nós'
+                }}
             />
         </stack.Navigator>
     )
